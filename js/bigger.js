216fbed4302ead10febd879c037c84999cfca9d8
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-17 10:51:13 -0500

    bigger of two

---
+++ js/bigger.js
@@ -0,0 +1,12 @@
+let a = 10, b=10;
+if(a>b){
+	console.log("a is greater");
+}
+else if(a<b){
+	console.log("b is greater");
+}
+// else if(){}
+// else if(){}
+else {
+	console.log("a equals b");
+}
