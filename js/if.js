9beae4f28f3e3bc8db3983526e0f646f59768702
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-17 10:51:33 -0500

    Create if.js

---
+++ js/if.js
@@ -0,0 +1,25 @@
+let a = 25;
+// if(condition){
+// 	//code to be executed
+// }
+
+// if(a<20){
+// 	console.log("a less than 20");
+// }
+// else {
+// 	console.log("a greater than 20");
+// }
+
+//nested if
+if(a>5){
+	if(a<20){
+		console.log("a lies between 5 and 20");
+	}
+	else{
+			console.log("greater than 20");
+	}
+}
+
+else{
+	console.log("less than 3");
+}
