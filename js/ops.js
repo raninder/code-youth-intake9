f9df6ec56708afdd38d486163c00859c74e92290
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-17 10:51:47 -0500

    operators

---
+++ js/ops.js
@@ -0,0 +1,11 @@
+let a = 20, b = 5, c = 3, d='4';
+a = a + 5;
+a += 5;
+c++;
+b--;
+console.log("a:",a,"b",b,"c",c);
+console.log("b equals d",b==d);
+console.log("logical and", a>5 && b>5);
+console.log("logical or", a>5 || b>5);
+console.log("not",!(a>10));
+console.log(typeof d);
