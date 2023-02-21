3fa237d7bd36339d87e42ed8fc5c48e39e7a2669
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-21 11:18:16 -0500

    Create for.js

---
+++ js/for.js
@@ -0,0 +1,31 @@
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+// console.log("hello");
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// ......
+
+// for(initlization;condition;updation){
+// 	//code to be executed
+// }
+
+for(i=1;i<=5;i++){
+	console.log("hello");
+}
+console.log("end")
+
+// i=1  i<=5 hello   i++
+// i=2 i<=5  hello   i++
+// i=3 i<=5  hello  i++
+// i=4   i<=5 hello i++
+// i=5  i<=5 hello i++
+// i=6  i<=5
+// come out of loop
+// end
+
+
