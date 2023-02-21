6a0260f355a053c4a9faa29109b809536211ebf7
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-21 11:18:33 -0500

    display natural number

---
+++ js/natural-for.js
@@ -0,0 +1,30 @@
+// output:
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+//first 10 natural nuymebrs
+for(let i=1;i<=10;i++){  //i=i+1//i++
+	console.log(i);
+}//n
+
+//first 10 odd natural nuumebrs
+console.log("odd natural numbers")
+for(let i=1;i<20;i+=2){    //i=i+2  i+=2
+	console.log(i);
+} 
+//first 10 odd natural nuumebrs
+console.log("even natural numbers")
+for(let i=2;i<=20;i+=2){    //i=i+2  i+=2
+	console.log(i);
+}
+
+O(3n)
+O(n)
