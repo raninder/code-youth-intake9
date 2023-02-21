af5472537ed54fe42af5caf244fcd399139ddc93
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-21 11:18:37 -0500

    Update ops.js

--- js/ops.js
+++ js/ops.js
@@ -1,5 +1,5 @@
-let a = 20, b = 5, c = 3, d='4';
-a = a + 5;
+let a = 20, b = 5, c = 3, d='4';  //1
+a = a + 5;  //1
 a += 5;
 c++;
 b--;
@@ -9,3 +9,5 @@ console.log("logical and", a>5 && b>5);
 console.log("logical or", a>5 || b>5);
 console.log("not",!(a>10));
 console.log(typeof d);
+
+// O(1)
