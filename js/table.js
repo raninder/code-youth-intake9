a9e57443a2f918c515960e9b27af3ead064cfe25
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-22 10:51:09 -0500

    multiplication table of a number

---
+++ js/table.js
@@ -0,0 +1,16 @@
+// program that prompts the user to input a positive integer. It should then print the multiplication table of that number. 
+let num = 5;
+let i=1;
+while(i<=10){
+	console.log(num,"X",i,"=",num*i);
+	i++;
+}
+
+
+
+
+
+// 5 x 1 = 5
+// 5 x 2 = 10
+// 5 x 3 = 15
+// ...
