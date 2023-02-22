05c66243fda29678e192d0a5cd58a2ae214f7207
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-22 10:51:25 -0500

    Create while.js

---
+++ js/while.js
@@ -0,0 +1,16 @@
+// for(initialization;condition;updation){
+// 	//body
+// }
+
+// initialization;
+// while(condition){
+// 	//body
+// 	updation;
+// }
+
+let i=1;
+while(i<10){
+	console.log(i);
+	i++;
+
+}
