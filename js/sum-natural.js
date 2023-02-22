b6c0a086c449eceea0b0561bd53d2aae3b147a20
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-22 10:50:55 -0500

    Update sum-natural.js

--- js/sum-natural.js
+++ js/sum-natural.js
@@ -6,13 +6,14 @@
 // c=a+b=3
 let sum=0;
 
-for(let i=1;i<=10;i++){
-sum =sum + i;  // 0  + 1  =1
+
+for(let i=1;i<=5;i++){
+	let num = prompt("enter a number");
+sum =sum + num;  // 0  + 1  =1
 			// 0   + 1  = 1
 			// 1   + 2   = 3
 			// 3  + 3  =6
 			// 6 + 4  = 10
 console.log("partial sum-", sum)
-
 }
 console.log("sum:",sum);
