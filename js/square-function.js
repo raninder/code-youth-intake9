fc2c7a3f759052e70d6317721695c4c5db44e810
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-24 10:54:24 -0500

    Update square-function.js

--- js/square-function.js
+++ js/square-function.js
@@ -1,10 +1,16 @@
 // 5  === 5*5
-
-function square(num){
-	let res = num*num;
+// let num=45
+function square(num1){
+	let res = num1*num1;
+	return res;
 	console.log("square of a number:",res);
 }
 
-square(5);
+console.log(square(5));
+let output = square(40);
+console.log(output);
 square(45);
 square(3456);
+
+
+//check whether no. os even or not  iseven(7)  true or false
