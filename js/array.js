2961d6e2fca6014baed43c60226afd3972d585b1
Raninder Dhillon <raninderdhillon@gmail.com>
2023-02-24 10:53:42 -0500

    arrays

---
+++ js/array.js
@@ -0,0 +1,39 @@
+let marks1 = 20, marks2 = 30, marks3 = 25, marks4 = 23, marks5 = 10;
+let marks = [20,30,25,23,10];
+marks[0]  //20
+marks[4]  //10
+
+//array literal method
+let data = ['john', 20, 'toronto', true]
+
+let arr = [];
+arr[0] = 10;
+arr[1] = 20;
+arr[4] = 40;
+console.log(arr);
+
+
+//array constructor method
+let arr1 = new Array(10,20,30,40,50);
+console.log(arr1)
+
+// array size: marks.length
+console.log("data array size",data.length);
+data[data.length-1]
+
+// Ex: array
+// colors
+// display 1st Element
+// 4the element
+// last Element
+// array size
+// complete array
+
+let colors = ['red','green','blue','yellow','orange','black','white','purple'];
+console.log("first element", colors[0]);
+console.log("4th element", colors[3]);
+console.log("array size", colors.length);
+console.log("last elemnt", colors[colors.length-1]);
+console.log("arrar elememnts are:", colors);
+
+
---
+++ js/array1.js
@@ -0,0 +1,11 @@
+let score = [10,12,45,23,34];
+0,1,2
+score[0]
+
+i
+score[i]
+for(let i=0;i<score.length;i++){
+	console.log(score[i]+2);
+	score[i] = score[i] + 2;
+}
+console.log(score);
