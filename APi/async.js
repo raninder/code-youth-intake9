6103f784b7766c4ead4197cfdd4125b9b3b9b2a3
Raninder Dhillon <raninderdhillon@gmail.com>
2023-03-10 10:51:37 -0500

    async and sync

---
+++ APi/async.js
@@ -0,0 +1,12 @@
+
+//synchronous programming
+// console.log("first");
+// console.log("second");
+// console.log("third");
+
+
+//asynchronous programming
+console.log("first");
+setTimeout(()=> console.log("second"), 200)
+
+console.log("third");
