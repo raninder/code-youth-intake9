b9c77f43047aefe795506901a6e05c4b344e6b54
Raninder Dhillon <raninderdhillon@gmail.com>
2023-03-31 11:07:06 -0400

    Update db_server.js

--- express/db_server.js
+++ express/db_server.js
@@ -3,15 +3,16 @@ const app = express()
 // const {getUsers,getUserById} = require('./db')
 const db = require('./db')
 
-app.get('/users',db.getUsers)
-app.get('/users/:id',db.getUserById)
+app.get('/users',db.getUsers) //postman api: localhost:8080/users
+
+app.get('/users/:id',db.getUserById) //postman api: localhost:8080/users/2
 // app.post('/users',db.createUser)
 app.set("view engine","ejs")
 app.get('/',(req,res)=>
 res.render('form'))
 
 app.use(express.urlencoded());
-app.post('/save',db.createUser);
+app.post('/save',db.createUser);//postman api: localhost:8080/save
 
 
 app.listen(8080,()=>console.log("server running"))
